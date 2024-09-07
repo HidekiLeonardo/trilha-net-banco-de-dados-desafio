99c3f3edec410dd7748013f554de2e4f5ddd33b4
HidekiLeonardo <leonardohideki778@gmail.com>
2024-09-07 19:46:58 -0300

    Descrição clara das suas alterações

---
+++ resolucao-sql.sql
@@ -0,0 +1,44 @@
+-- Buscar o nome e ano dos filmes
+SELECT Nome, Ano From Filmes
+
+-- Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+SELECT Nome, Ano FROM Filmes ORDER BY Ano ASC
+
+-- Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+SELECT Nome, Ano, Duracao FROM Filmes WHERE UPPER(Nome) = UPPER('de volta para o futuro')
+
+-- Buscar os filmes lançados em 1997
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano = 1997
+
+-- Buscar os filmes lançados APÓS o ano 2000
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Ano > 2000
+
+-- Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Duracao > 100 AND Duracao < 150 ORDER BY Duracao ASC
+
+-- Buscar os filmes com a duracao maior que 100 e menor que 150, ordenando pela duracao em ordem crescente
+SELECT Ano, COUNT(*) AS Quantidade FROM Filmes GROUP BY Ano ORDER BY Quantidade DESC
+
+-- Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT * FROM Atores WHERE Genero = 'M'
+
+-- Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT * FROM Atores WHERE Genero = 'F' ORDER BY PrimeiroNome ASC
+
+--Buscar o nome do filme e o gênero
+SELECT Filmes.Nome, Generos.Genero FROM Filmes 
+INNER JOIN FilmesGenero ON FilmesGenero.IdFilme = Filmes.Id
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+SELECT * FROM Filmes
+SELECT * FROM Generos
+
+-- Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT Filmes.Nome, Generos.Genero FROM Filmes 
+INNER JOIN FilmesGenero ON FilmesGenero.IdFilme = Filmes.Id
+INNER JOIN Generos ON Generos.Id = FilmesGenero.IdGenero
+WHERE Generos.Genero = 'Mistério'
+
+-- Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT Filmes.Nome, Atores.PrimeiroNome, Atores.UltimoNome, ElencoFilme.Papel FROM Filmes
+INNER JOIN ElencoFilme ON ElencoFilme.IdFilme = Filmes.Id
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
